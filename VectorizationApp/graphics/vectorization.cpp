b06b928f458ab05696d038b14210380f9f716ffe
YulyIllarionov <yyurikedd@gmail.com>
2016-02-22 16:55:29 +0300

    Добавлен поиск линий на скелетизированном изображении

--- VectorizationApp/graphics/vectorization.cpp
+++ VectorizationApp/graphics/vectorization.cpp
@@ -15,25 +15,42 @@ Vectorization::~Vectorization ()
 }
 
 Vectorization::Vectorization() :
-  imageRaster(NULL), imageRasterTemp(NULL) //TODO не уверен в NULL, инициализация lines
+  m_imageRaster(NULL), m_imageRasterTemp(NULL) //TODO не уверен в NULL, инициализация m_lines
 {
 }
 
 Vectorization::Vectorization(WImageRaster* image) :
-  imageRaster(image), imageRasterTemp(image)
+  m_imageRaster(image), m_imageRasterTemp(image)
 {
 
 }
 
 void Vectorization::onSkeleton()
 {
-	for (int x = 0; x < imageRasterTemp->getWidth(); x++)
+	for (int x = 0; x < m_imageRasterTemp->getWidth(); x++)
 	{
-		for (int y = 0; y < imageRasterTemp->getHeight(); y++)
-		{
-			if(imageRasterTemp->at(x,y) != WCOLOR_WHITE)
+		for (int y = 0; y < m_imageRasterTemp->getHeight(); y++)
+		{ 
+			if(m_imageRasterTemp->at(x,y) != WCOLOR_WHITE)
 			{
-
+                WLine lineClockwise;
+                WPoint current(x,y);
+                while(!current.IsEmpty())
+                {
+                    lineClockwise.AddPoint(current);
+                    m_imageRasterTemp->at(current)=WCOLOR_WHITE;
+                    current=m_imageRasterTemp->NeihborClockwise(current);
+                }
+                WLine lineCounterClockwise;
+                current = WPoint(x,y);
+                while(!current.IsEmpty())
+                {
+                    lineCounterClockwise.AddPoint(current);
+                    m_imageRasterTemp->at(current)=WCOLOR_WHITE;
+                    current=m_imageRasterTemp->NeihborCounterClockwise(current);
+                }
+                lineClockwise.Concat(lineCounterClockwise);
+                m_lines.push_back(lineClockwise);
 			}
 		}
 	}
