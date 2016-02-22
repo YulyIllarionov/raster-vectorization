b30fc9123372e610cb0d6719427f75e093eb24f7
zkoalexey <zkoalexey@gmail.com>
2016-02-22 19:21:53 +0300

    Запись в Dxf

---
+++ VectorizationApp/WDxfWriter.h
@@ -0,0 +1,23 @@
+#ifndef _WDXFWRITER_H_
+#define _WDXFWRITER_H_
+#pragma once
+#include "base_types.h"
+#include "libs\dl_dxf.h"
+using namespace std;
+
+APP_BEGIN_NAMESPACE
+class WDxfWriter
+{
+public:
+    WDxfWriter(char *filename);
+    ~WDxfWriter();
+    void begin();
+    void end();
+    void writePolyLine(WLine line);
+    DL_Dxf dxf;
+    DL_WriterA* dw;
+private:
+    string filename;
+};
+APP_END_NAMESPACE
+#endif
